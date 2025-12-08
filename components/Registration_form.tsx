dea7b97eced7991791582ac579798f2dccccd1af
Abhineetsahay <abhineetsahay@gmail.com>
2025-12-09 01:08:26 +0530

    bas chal jaye ab part -4

--- components/Registration_form.tsx
+++ components/Registration_form.tsx
@@ -123,11 +123,11 @@ const Registration_form = () => {
       localStorage.setItem("id", data.saved._id);
       localStorage.setItem("isRegistrated", "true");
 
-      // fetch("/api/save-to-sheet", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify(data.saved),
-      // }).catch((err) => console.log("Sheet Sync Error:", err));
+      fetch("/api/save-to-sheet", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data.saved),
+      }).catch((err) => console.log("Sheet Sync Error:", err));
 
       toast.success("Successfully registered");
       setTimeout(() => {
