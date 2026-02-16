c6fcbd780a2a2dcfd7771106c8402f880ae80b4b
phaANTom2303 <anikavy.goenka@gmail.com>
2026-02-17 01:08:37 +0530

    added information panel

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import background from "@/public/trapped-bg.png";
 import GalleryCarousel from "@/components/GalleryCarousel";
+import InfoPanel from "@/components/InfoPanel";
 
 const whatsappGroupLink="https://chat.whatsapp.com/DaeP2oi13EE9kRT5GeCjf1"
 // Navbar links
@@ -201,6 +202,8 @@ const Home = () => {
             </div>
           </motion.div>
         </div>
+
+        <InfoPanel />
 
         <GalleryCarousel />
       </main>
